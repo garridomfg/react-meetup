0e99dd17234031f9722988c008b2ec361a1d50a3
garridomfg <garridomfg@gmail.com>
2022-08-23 20:40:37 +0200

    add animation to navbar

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,25 +1,36 @@
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
-export const MainNavigation = ({ setPage }) => {
+export const MainNavigation = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const scrollControll = () => {
+      const scrollPosition = window.pageYOffset;
+      setVisible(prevScrollPos > scrollPosition);
+      setPrevScrollPos(scrollPosition);
+    };
+    window.addEventListener("scroll", scrollControll);
+
+    return () => window.removeEventListener("scroll", scrollControll);
+  }, [prevScrollPos, visible]);
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header
+      className={`${visible && classes.header}`}
+      data-test="navigation-header"
+    >
       <Link to="/" className={classes.logo}>
         React Meetups
       </Link>
       <nav>
-        <ul>
-          <li>
-            <NavLink to="/">All Meetups</NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/new-meetup"> Add New Meetup</NavLink>
-          </li>
-          <li>
-            <NavLink to="/favorites">My Favorites</NavLink>
-          </li>
-        </ul>
+        <NavLink to="/">All Meetups</NavLink>
+        <NavLink to="/new-meetup"> Add New Meetup</NavLink>
+        <NavLink to="/favorites">
+          My Favorites<span className={classes.badge}>{0}</span>
+        </NavLink>
       </nav>
     </header>
   );
